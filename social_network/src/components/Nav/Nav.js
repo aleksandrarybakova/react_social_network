a8115dab018431b1d75df74db02732b3de8c67c8
Aleksandrarybakova <sashik1988@yandex.ru>
2021-02-10 19:40:32 +0300

    NavLink

--- social_network/src/components/Nav/Nav.js
+++ social_network/src/components/Nav/Nav.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 const Nav = () => {
     return (
         <nav className="nav">
             <div>
-                <a href="/profile">profile</a>
+                <NavLink to="/profile">profile</NavLink>
             </div>
             <div>
-                <a href="/dialogs">messages</a>
+                <NavLink to="/dialogs">messages</NavLink>
             </div>
             <div>
-                <a href="/news">news</a>
+                <NavLink to="/news">news</NavLink>
             </div>
             <div>
-                <a href="/music">music</a>
+                <NavLink to="/music">music</NavLink>
             </div>
             <div>
-                <a href="/settings">settings</a>
+                <NavLink to="/settings">settings</NavLink>
             </div>
         </nav>
     )
